890de0ade49a4ae20806123edf27df8256e4c53d
Harshil Modi <harshilmodi014@gmail.com>
2022-12-24 20:49:09 +0530

    2nd

--- Website/app.py
+++ Website/app.py
@@ -1,7 +1,35 @@
 import streamlit as st
 import pickle
-import pandas as pd
 import requests
+import base64
+
+
+st.set_page_config(layout="wide")
+
+hide_img_fs = '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
+
+st.markdown(hide_img_fs, unsafe_allow_html=True)
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('BG Image/bg7.jpg')
 
 movies = pickle.load(open('movies.pkl', 'rb'))
 movie_list = movies['title'].values
